496dfe5b7d48095fc55ba648f747b4299ae3500d
masayuki oguni <masayuki.oguni.dev@gmail.com>
2012-06-18 00:24:00 +0900

    end 3st

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -5,17 +5,17 @@ require './message_filter'
    share_examples_for 'MessageFilter with argument "foo"' do
      it { should be_detect('hello from foo') }
      it { should_not be_detect('hello, world!') }
+     its(:ng_words) { should_not be_empty }
    end
    context 'with argument "foo"' do
      subject { MessageFilter.new('foo') }
      it_should_behave_like 'MessageFilter with argument "foo"'
-     it 'ng_words should not be empty' do
-       subject.ng_words.empty?.should == false
-    end
+     it { should have(1).ng_words }
    end
    context 'with argument "foo","bar"' do
      subject { MessageFilter.new('foo', 'bar') }
      it { should be_detect('hello from bar') }
      it_should_behave_like 'MessageFilter with argument "foo"'
+     it { should have(2).ng_words }
    end
  end
